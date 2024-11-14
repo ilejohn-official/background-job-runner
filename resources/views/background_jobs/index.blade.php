b8136767365fe1e87412a9313974bf7aa7dfdb75
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2024-11-14 18:29:46 +0100

    logs and log view

---
+++ resources/views/background_jobs/index.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Background Jobs Dashboard') }}
+        </h2>
+    </x-slot>
+
+    @if (session('status'))
+        <p
+            x-data="{ show: true }"
+            x-show="show"
+            x-transition
+            x-init="setTimeout(() => show = false, 2000)"
+            class="text-sm text-gray-600"
+        >{{ session('status') }}</p>
+    @endif
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Job ID</th>
+                    <th>Class</th>
+                    <th>Method</th>
+                    <th>Status</th>
+                    <th>Attempts</th>
+                    <th>Priority</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($jobs as $job)
+                    <tr>
+                        <td>{{ $job['id'] }}</td>
+                        <td>{{ $job['class'] }}</td>
+                        <td>{{ $job['method'] }}</td>
+                        <td>{{ $job['status'] }}</td>
+                        <td>{{ $job['attempts'] }}</td>
+                        <td>{{ $job['priority'] }}</td>
+                        <td>
+                            <a href="{{ route('background_jobs.logs', $job['id']) }}" class="btn btn-info">View Logs</a>
+                            @if($job['status'] !== 'canceled' && $job['status'] !== 'completed')
+                                <form action="{{ route('background_jobs.cancel', $job['id']) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    <x-danger-button class="ms-4">Cancel</x-danger-button>
+                                </form>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        </div>
+    </div>
+</x-app-layout>
