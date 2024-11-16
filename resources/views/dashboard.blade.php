9cf7b4618f3a08442be036a964dcc2fd6ff50a38
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2024-11-16 09:39:44 +0100

    show link to background job

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,20 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto my-2 sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <a href="{{ route('background-jobs.index') }}" class="mb-4 text-blue-500 hover:text-blue-700">
+                         Go to Background Job Dashboard
+                    </a>
                 </div>
             </div>
         </div>
