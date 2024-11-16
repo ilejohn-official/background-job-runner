3e2cbc5adb0045353e3f3843a5798642a1c77bd7
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2024-11-16 09:17:55 +0100

    adjust prefix issues

--- app/Services/DashboardService.php
+++ app/Services/DashboardService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
 class DashboardService
@@ -12,14 +13,19 @@ class DashboardService
      * @return array List of all jobs with their statuses.
      */
     public static function getAllJobStatuses()
-    {
+    {        
         // Fetch all job status keys
         $jobKeys = Redis::keys(BackgroundJobRunner::JOB_STATUS_PREFIX . '*');
         $jobs = [];
 
         foreach ($jobKeys as $key) {
-            $jobData = Redis::hgetall($key);
-            $jobs[] = array_merge(['id' => str_replace(BackgroundJobRunner::JOB_STATUS_PREFIX, '', $key)], $jobData);
+            // Remove the Redis prefix (if any) and the JOB_STATUS_PREFIX
+            $rawKey = str_replace(config('database.redis.options.prefix'), '', $key);
+            $jobId = str_replace(BackgroundJobRunner::JOB_STATUS_PREFIX, '', $rawKey);
+
+            // Retrieve the job data and merge with the raw job ID due to laravel prefixing
+            $jobData = Redis::hgetall($rawKey);
+            $jobs[] = array_merge(['id' => $jobId], $jobData);
         }
 
         return $jobs;
